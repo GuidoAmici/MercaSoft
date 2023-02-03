49ee4842585e89b2a80d7a6b828791483bfd4b68
Guido Amici <guido.amici95@gmail.com>
2023-02-03 00:27:07 -0300

    Added function to insert Forms into MainPanel

--- View/Container.cs
+++ View/Container.cs
@@ -9,13 +9,47 @@ namespace View
 
         private void Container_Reload(object sender, EventArgs e)
         {
-            /*
-            if (!Program.Logged)
+            if (Program.Logged == false)
             {
-                this.mainPanel.Parent = new LogIn();
-                this.mainPanel.Show();
+                sidePanel.Visible = false;
+                InsertIntoPanel<Login>();
             }
-            */
+            else
+            {
+                sidePanel.Visible = true;
+            }
+        }
+
+        private void InsertIntoPanel<InsertedForm>() where InsertedForm : Form, new()
+        {
+            Form formInPanel;
+            formInPanel = mainPanel.Controls.OfType<InsertedForm>().FirstOrDefault();
+
+            if (formInPanel != null)
+            {
+                formInPanel.BringToFront();
+                if (formInPanel.WindowState == FormWindowState.Minimized)
+                {
+                    formInPanel.WindowState = FormWindowState.Normal;
+                }
+            }
+            else
+            {
+                formInPanel = new InsertedForm();
+                formInPanel.TopLevel = false;
+                formInPanel.FormBorderStyle = FormBorderStyle.None;
+                formInPanel.Dock = DockStyle.Fill;
+                mainPanel.Controls.Add(formInPanel);
+                mainPanel.Tag = formInPanel;
+                formInPanel.Show();
+
+                formInPanel.BringToFront();
+            }
+        }
+
+        private void FormInPanel_FormClosed(object? sender, FormClosedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
