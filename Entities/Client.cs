c68f4d2874af56cb57d6fdf7fcedb659f00234c1
Guido Amici <guido.amici95@gmail.com>
2023-02-02 22:22:14 -0300

    Created 2 basic entities

--- Entities/Client.cs
+++ Entities/Client.cs
@@ -2,22 +2,11 @@
 {
     public class Client
     {
+        public int ID { get; set; }
         private string Name { get; set; }
-        private string LastName { get; set; }
-        private int DNI { get; set; }
-        private string Birth { get; set; }
-
 
         private Client()
         {
-        }
-
-        private Client(string name, string lastName, int dni, string birth)
-        {
-            Name = name;
-            LastName = lastName;
-            DNI = dni;
-            Birth = birth;
         }
 
     }
---
+++ Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    internal class User
+    {
+        private int ID { get; set; }
+        private string Name { get; set; }
+        private string Password { get; set; }
+        private string Email { get; set; }
+    }
+}
