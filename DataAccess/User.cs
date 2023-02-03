cf74075c11cee833ed1fddb70f3955e22662f751
Guido Amici <guido.amici95@gmail.com>
2023-02-03 03:51:50 -0300

    Added unfinished Control layer class User

---
+++ DataAccess/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control
+{
+    public static class User
+    {
+        public static bool LogIn(string user, string password)
+        {
+            //UNDONE verificar datos en la DB y devolver true o false.
+            //TODO Cargar Program.User en caso de que exista el usuario
+            //IDEA Podemos crear un Log History
+            return true;
+        }
+    }
+}
