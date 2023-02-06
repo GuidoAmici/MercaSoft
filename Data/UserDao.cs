c970b0f28e96b68350c3b8ac1b2c903a32a2d88e
Guido Amici <guido.amici95@gmail.com>
2023-02-06 06:37:41 -0300

    Added GetUser function to UserDao

---
+++ Data/UserDao.cs
@@ -0,0 +1,46 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control
+{
+    public class UserDao
+    {
+        public User GetUser(string username, string password)
+        {
+            DAO dao = new DAO();
+            string CheckLogInData = "select * from Users where User = " + username + " and Password = " + password;
+            User user = null;
+
+            try
+            {
+                dao.OpenConnection();
+                dao.SetConsult(CheckLogInData);
+                dao.ExecuteConsult();
+                if (dao.Read() != null)
+                {
+                    user = new User(
+                        (int)dao.Read()["ID"],
+                        (string)dao.Read()["Username"],
+                        (string)dao.Read()["Password"],
+                        (string)dao.Read()["Email"]
+                        );
+
+                }
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                dao.CloseConnection();
+            }
+        }
+    }
+}
