0f01d1909a6c4ba32736758967cc79a9a9be4b31
Guido Amici <guido.amici95@gmail.com>
2023-02-06 06:32:25 -0300

    Added static class Sesion to Control layer

---
+++ Control/Sesion.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control
+{
+    public static class Sesion
+    {
+        internal static User? User;
+
+        public static bool SesionStatus()
+        {
+            if (User == null) return false;
+            else return true;
+        }
+
+        public static bool LogIn(string username, string password)
+        {
+            UserDao userDao = new UserDao();
+            User = userDao.GetUser(username, password);
+            if (User == null) return false;
+            else return true;
+        }
+
+        public static void LogOut()
+        {
+            User = null;
+        }
+    }
+}
