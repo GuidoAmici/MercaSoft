d0671d57ed2a8164f7843b5002d97c148e4192ea
Guido Amici <guido.amici95@gmail.com>
2023-02-08 02:13:52 -0300

    Added Description, CodeName and BarCode to class Item

--- Entities/Item.cs
+++ Entities/Item.cs
@@ -4,6 +4,9 @@
     {
         public int ID { get; internal set; }
         public string Name { get; internal set; }
+        public string Description { get; internal set; }
+        public string CodeName { get; internal set; }
+        public int BarCode { get; internal set; }
         public float Price { get; internal set; }
 
         public Item() { }
