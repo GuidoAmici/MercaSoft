1adcacfd79f71c1994cf447826327b746a4d1104
Guido Amici <guido.amici95@gmail.com>
2023-02-08 02:49:33 -0300

    Added Category class to match DB table

---
+++ Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities
+{
+    public class Category
+    {
+        public int ID { get; internal set; }
+        public string Name { get; internal set; }
+        public bool IsActive { get; internal set; }
+    }
+}
