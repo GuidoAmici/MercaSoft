26255fa22803428a2b511b36c8908614d75ddc69
Guido Amici <guido.amici95@gmail.com>
2023-02-09 22:03:34 -0300

    Added Info static class and minor changes

---
+++ Control/Info.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using Data;
+
+namespace Control
+{
+    public static class Info
+    {
+        public static List<Category> GetCategories(bool isActive)
+        {
+            List<Category> list = CategoryDao.GetCategories();
+            if (isActive)
+            {
+                foreach (Category obj in list)
+                {
+                    if (!obj.IsActive)
+                    {
+                        list.Remove(obj);
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
