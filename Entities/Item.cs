4038bca8383114206eb56214b17368fa2cbd51df
Guido Amici <guido.amici95@gmail.com>
2023-03-16 19:11:31 -0300

    Minor changes in Item entity

--- Entities/Item.cs
+++ Entities/Item.cs
@@ -11,18 +11,33 @@
         public string? Description { get; internal set; }
         public string? CodeName { get; internal set; }
         public int? BarCode { get; internal set; }
+        public int? SuppliedQuantity { get; set; }
 
-        public Item(int id, string name, bool isforsale, float price,
+        public Item(int id, string name, bool isforsale, float price, int stock,
             Category category, string description, string codename, int barcode)
         {
             ID = id;
             Name = name;
-            Price = price;
             IsForSale = isforsale;
+            Price = price;
+            Stock = stock;
             Category = category;
             Description = description;
             CodeName = codename;
             BarCode = barcode;
+        }
+
+        public Item(Item item)
+        {
+            ID = item.ID;
+            Name = item.Name;
+            IsForSale = item.IsForSale;
+            Price = item.Price;
+            Stock = item.Stock;
+            Category = item.Category;
+            Description = item.Description;
+            CodeName = item.CodeName;
+            BarCode = item.BarCode;
         }
 
         public override string ToString()
