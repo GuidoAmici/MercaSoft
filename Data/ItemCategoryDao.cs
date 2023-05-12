55a4c3f6db48839cff6f3898183d7aaaaeae5d46
Guido Amici <guido.amici95@gmail.com>
2023-05-12 04:37:28 -0300

    Category renamed as ItemCategory

---
+++ Data/ItemCategoryDao.cs
@@ -0,0 +1,65 @@
+﻿using Control;
+using Entities;
+
+namespace Data
+{
+    public static class ItemCategoryDao
+    {
+        public static List<ItemCategory> Get()
+        {
+            DAO dao = new();
+            string query = "select * from ItemCategories";
+            List<ItemCategory> list = new();
+            ItemCategory obj;
+
+            try
+            {
+                dao.OpenConnection();
+                dao.SetConsult(query);
+                dao.ExecuteConsult();
+
+                while (dao.Reader.Read())
+                {
+                    obj = new(
+                        (int)dao.Reader["ID"],
+                        (string)dao.Reader["Name"],
+                        (bool)dao.Reader["IsActive"]
+                        );
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+            catch (Exception) { throw; }
+            finally { dao.CloseConnection(); }
+        }
+
+        public static ItemCategory Get(int itemCategoryID)
+        {
+            DAO dao = new();
+            string query = $"select * from ItemCategories where ID = {itemCategoryID}";
+            List<ItemCategory> list = new();
+            ItemCategory obj;
+
+            try
+            {
+                dao.OpenConnection();
+                dao.SetConsult(query);
+                dao.ExecuteConsult();
+
+                dao.Reader.Read();
+
+                obj = new(
+                    (int)dao.Reader["ID"],
+                    (string)dao.Reader["Name"],
+                    (bool)dao.Reader["IsActive"]
+                    );
+
+                return obj;
+            }
+            catch (Exception) { throw; }
+            finally { dao.CloseConnection(); }
+        }
+    }
+}
