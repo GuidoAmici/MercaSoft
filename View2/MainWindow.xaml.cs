cb635b140d933273ab08e7fc973a5f30ae633ff1
Guido Amici <guido.amici95@gmail.com>
2023-07-17 00:40:23 -0300

    Added WPF front end as View2

---
+++ View2/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace View2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuDashboard_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
